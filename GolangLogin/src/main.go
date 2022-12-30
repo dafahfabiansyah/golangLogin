a9aae10b690a06af4ef0f7e36095c831b6415591
dafahfabiansyah <121594681+dafahfabiansyah@users.noreply.github.com>
2022-12-31 05:48:11 +0700

    Update main.go

--- GolangLogin/src/main.go
+++ GolangLogin/src/main.go
@@ -8,11 +8,19 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", authcontroller.Index)
-	http.HandleFunc("/login", authcontroller.Login)
-	http.HandleFunc("/logout", authcontroller.Logout)
-	http.HandleFunc("/register", authcontroller.Register)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", authcontroller.Index)
+	mux.HandleFunc("/login", authcontroller.Login)
+	mux.HandleFunc("/logout", authcontroller.Logout)
+	mux.HandleFunc("/register", authcontroller.Register)
 
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("Server jalan di: http://localhost:3000")
-	http.ListenAndServe(":3000", nil)
 }
